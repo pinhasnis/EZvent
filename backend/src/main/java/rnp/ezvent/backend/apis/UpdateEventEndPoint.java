1d6b6b5e3d40b392a3bef7e2b38b48febf60e0ce
Pinhas <pinhasn@gmail.com>
2016-05-05 19:37:10 +0300

    Merge branch 'master' of https://github.com/pinhasnis/EZvent
    # Conflicts:
    #	backend/src/main/java/rnp/ezvent/backend/utils/MySQL_Util.java

--- backend/src/main/java/rnp/ezvent/backend/apis/UpdateEventEndPoint.java
+++ backend/src/main/java/rnp/ezvent/backend/apis/UpdateEventEndPoint.java
@@ -137,7 +137,7 @@ public class UpdateEventEndPoint {
                 ArrayList<String[]> vals = new ArrayList<>();
                 for (String[] arr: update[Constants.update_event_update]) {
                     String[] newArr = new String[where_delete.size()];
-                    for (int j = 0; j < newArr.length; j++) {
+                    for (int j = 0; j < newArr.length-1; j++) { // minus one because of event-id!
                         newArr[j] = arr[j];
                     }
                     vals.add(newArr);
