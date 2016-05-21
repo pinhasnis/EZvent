a679c82203c3e32cbbed996e67e32cbcdfd33a8b
Pinhas <pinhasn@gmail.com>
2016-05-21 11:22:42 +0300

    Merge branch 'master' of https://github.com/pinhasnis/EZvent
    # Conflicts:
    #	backend/src/main/java/rnp/ezvent/backend/utils/MySQL_Util.java

--- app/src/main/java/server/Event/Event_AsyncTask_delete.java
+++ app/src/main/java/server/Event/Event_AsyncTask_delete.java
@@ -3,6 +3,8 @@ package server.Event;
 import android.content.Context;
 import android.os.AsyncTask;
 
+import java.util.Random;
+
 import rnp.backend.ezvent.Ezvent;
 
 import server.CloudEndpointBuilderHelper;
@@ -25,12 +27,44 @@ public class Event_AsyncTask_delete  extends AsyncTask<String, Void, Void> {
             myApiService = CloudEndpointBuilderHelper.getEndpoints();
         }
 
+        int backoff = Constants.BACKOFF_INITIAL_DELAY;
+        Random random = new Random();
+        boolean finish = true;
+        for (int attemp = 0; attemp < Constants.MaxSendAttemp; attemp++) {
+            finish = true;
+            try {
+                myApiService.deleteEvent(params[0], Constants.MY_User_ID).execute();
+            } catch (Exception e) {
+                finish = false;
+            }
+            finally {
+                if(finish){
+                    attemp = Constants.MaxSendAttemp;
+                }
+                else{
+                    try {
+                        int sleepTime = backoff / 2 + random.nextInt(backoff);
+                        Thread.sleep(sleepTime);
+                        if (2 * backoff < Constants.MAX_BACKOFF_DELAY) {
+                            backoff *= 2;
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        if(!finish){
+            //TODO in case message didn't send add it to some database and try again latter..
+        }
+
+/*
         try {
             myApiService.deleteEvent(params[0], Constants.MY_User_ID).execute();
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+*/
         return null;
     }
 
